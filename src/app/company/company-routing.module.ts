5771d9b027b1b3e7ad8ee2082994227097c31c4a
ThetaShis <todopassanomundo@gmail.com>
2020-05-08 18:40:33 +0900

    修正

--- src/app/company/company-routing.module.ts
+++ src/app/company/company-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
-import { PresskitComponent } from './presskit/presskit.component';
 
 const routes: Routes = [
-  { path: 'contact', component: ContactComponent },
-  { path: 'presskit', component: PresskitComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { CompanyRoutingModule } from './company-routing.module';
 import { ContactComponent } from './contact/contact.component';
-import { PresskitComponent } from './presskit/presskit.component';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../core/module/material/material.module';
@@ -11,7 +10,7 @@ import { SharedModule } from '../core/shared/shared.module';
 import { AngularFirebaseTemplateModule } from 'angular-firebase-template';
 
 @NgModule({
-  declarations: [ContactComponent, PresskitComponent],
+  declarations: [ContactComponent],
   imports: [
     CommonModule,
     CompanyRoutingModule,
