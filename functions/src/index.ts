08c3b159dbaf74a74f17c724885c78fcee82f99c
ThetaShis <todopassanomundo@gmail.com>
2020-05-11 10:35:31 +0900

    firebaseの設定完了

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,8 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import { mail } from "./mails";
+import * as functions from 'firebase-functions';
 
-admin.initializeApp({
-  credential: admin.credential.cert(
-    JSON.parse(
-      JSON.stringify(functions.config().service_account).replace(
-        /\\\\n/g,
-        "\\n"
-      )
-    )
-  ),
-  databaseURL: functions.config().admin.database_url
-});
-
-export const mail_send = mail.send;
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBk26w7GlfCrRiePtwhYjD2-bJ5N3kWFjA",
-    authDomain: "and-ath.firebaseapp.com",
-    databaseURL: "https://and-ath.firebaseio.com",
-    projectId: "and-ath",
-    storageBucket: "and-ath.appspot.com",
-    messagingSenderId: "719015932786",
-    appId: "1:719015932786:web:bac6639b47baabde06e3eb",
-    measurementId: "G-0DWWV0RRYD"
+    apiKey: "AIzaSyCCIc3JYpEvCKm4DYbH_z_1NcKZZSgNPjY",
+    authDomain: "and-ath-website.firebaseapp.com",
+    databaseURL: "https://and-ath-website.firebaseio.com",
+    projectId: "and-ath-website",
+    storageBucket: "and-ath-website.appspot.com",
+    messagingSenderId: "561685784928",
+    appId: "1:561685784928:web:dc7905e09f7793ec959605",
+    measurementId: "G-NZ2MMKBJNC"
   },
 };
