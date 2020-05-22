6c8a839fa36786a1333d50f34a2af8aeca482bd7
ThetaShis <todopassanomundo@gmail.com>
2020-05-22 22:29:23 +0900

    修正

--- functions/src/mails/module.ts
+++ functions/src/mails/module.ts
@@ -1,57 +1,62 @@
 import * as functions from "firebase-functions";
-import * as request from "request";
+
+const nodemailer = require("nodemailer");
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const adminEmail = functions.config().admin.email;
+
+// 送信に使用するメールサーバーの設定
+const mailTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+});
 
 export const send = functions.https.onCall(
-  async (
-    data: {
-      lang: string;
-      email: string;
-      name: string;
-      subject: string;
-      body: string;
-      type: "sales" | "support" | "info";
-    },
-    context
-  ) => {
-    const body =
-      data.lang === "en"
-        ? `Dear ${data.name}.
-Your inquiry has been submitted.
-Thanks for your inquiry.
+    async (
+      data: {
+        lang: string;
+        email: string;
+        name: string;
+        subject: string;
+        body: string;
+        type: "sales" | "support" | "info";
+      },
+      context
+    ) => {
+      const body =
+        data.lang === "en"
+          ? `Dear ${data.name}.
+  Your inquiry has been submitted.
+  Thanks for your inquiry.
+  
+  Inquiry details:
+  ${data.body}`
+          : `${data.name}様
+  お問い合わせを送信しました。
+  お問い合わせいただきありがとうございます。
+  
+  お問い合わせ内容:
+  ${data.body}`;
 
-Inquiry details:
-${data.body}`
-        : `${data.name}様
-お問い合わせを送信しました。
-お問い合わせいただきありがとうございます。
+  await new Promise<void>(() => {
 
-お問い合わせ内容:
-${data.body}`;
-
-    await new Promise<void>((resolve, reject) => {
-      request.post(
-        functions.config().gas.send_mail,
-        {
-          form: {
+        let adminMail = {
+            to: adminEmail,
             email: data.email,
             subject: data.subject,
             body: body,
             type: data.type
+        };
+
+        try {
+           mailTransport.sendMail(adminMail);
+        } catch (e) {
+            console.error(`send failed. ${e}`);
+            throw new functions.https.HttpsError('internal', 'send failed');
           }
-        },
-        (error, response, body_) => {
-          if (error) {
-            reject(error);
-            return;
-          }
-          resolve();
-        }
-      );
-    }).catch(_ => {
-      throw new functions.https.HttpsError(
-        "internal",
-        "failed to send an email"
-      );
     });
-  }
+    }
 );
