a63299d3e2e4b9c8dd2608b73384ebcaa2283963
ThetaShis <todopassanomundo@gmail.com>
2020-05-24 19:26:05 +0900

    シュウセイ

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,6 @@
-import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import { mail } from "./mails";
 
-admin.initializeApp({
-  credential: admin.credential.cert(
-    JSON.parse(
-      JSON.stringify(functions.config().service_account).replace(
-        /\\\\n/g,
-        "\\n"
-      )
-    )
-  ),
-  databaseURL: functions.config().admin.database_url
-});
+admin.initializeApp({});
 
 export const mail_send = mail.send;
