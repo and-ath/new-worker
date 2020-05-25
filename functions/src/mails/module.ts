c8db783f8eb34a2920daaeeca290c83e2a6b3bb2
ThetaShis <todopassanomundo@gmail.com>
2020-05-25 22:18:22 +0900

    修正

--- functions/src/mails/module.ts
+++ functions/src/mails/module.ts
@@ -5,31 +5,23 @@ const API_KEY = functions.config().sendgrid.key;
 
 sgMail.setApiKey(API_KEY);
 
-// 送信に使用するメールサーバーの設定
-
-export const sendEmail = functions.region('asia-northeast1').https.onCall(
-    (data, context) => {
-        return (content: {
-            to: string;
-            cc: string;
-            from: string;
-            subject: string;
-            templateId: string;
-            dynamic_template_data: {};
-        }) => {
-            content.to = data.name;
-            content.cc = 'info@and-ath.com';
-            content.from = 'noreply@and-ath.com';
-            content.subject = '株式会社&ath';
-            content.templateId = 'd-36df7bcc51cc4417ba6169091c72e730';
-            content.dynamic_template_data = {
+export const sendEmail = functions.https.onCall(
+  async (data, context) => {
+        const content = {
+            to : data.name,
+            cc : 'info@and-ath.com',
+            from : 'noreply@and-ath.com',
+            subject : '株式会社&ath',
+            templateId : 'd-36df7bcc51cc4417ba6169091c72e730',
+            dynamic_template_data : {
                 name: data.name,
                 subject: data.subject,
                 body: data.body,
-            };
+            }
+          }
           console.log(sgMail);
           console.log(content);
-          return sgMail.send(content); 
+          const result = await sgMail.send(content);
+          return result
         }
-      }
     );
